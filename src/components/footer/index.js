abd12cf27c66305d3a2e6b6ad62855f81a0002ee
mahesh <dev030.rejoice@gmail.com>
2026-02-03 17:42:23 +0530

    styles:some design issue fixed

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -14,43 +14,45 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className='container-lg'>
-        <div className={styles.footerTopAlignment}>
-          <div className={styles.footerlogo}>
-            <Link href="/">
-              <img src={FooterLogo} alt='FooterLogo' />
-            </Link>
-            <p >
-              Copyright © Helix
-            </p>
+        <div style={{ position: "relative" }}>
+          <div className={styles.footerTopAlignment}>
+            <div className={styles.footerlogo}>
+              <Link href="/">
+                <img src={FooterLogo} alt='FooterLogo' />
+              </Link>
+              <p >
+                Copyright © Helix
+              </p>
+            </div>
+            <div className={styles.footerMenu}>
+              <a onClick={() => { handleClick("industries"); }}>Industries</a>
+              <a onClick={() => { handleClick("workflow"); }}>Workflow</a>
+              <a onClick={() => { handleClick("pricing"); }}>Pricing</a>
+              <a href='https://platform.runhelix.ai/auth' target='_blank'>Login</a>
+            </div>
+            <div className={styles.socialIcon}>
+              <a href='https://www.tiktok.com/notfound' target='_blank'>
+                <img src={TiktokIcon} alt='TiktokIcon' />
+              </a>
+              <img src={TwitterIcon} alt='TwitterIcon' />
+              <a href='https://www.linkedin.com/company/run-helix-ai' target='_blank'>
+                <img src={LinkdinIcon} alt='LinkdinIcon' />
+              </a>
+              <a href='https://www.youtube.com/@RunHelix_AI' target='_blank'>
+                <img src={YoutubeIcon} alt='YoutubeIcon' />
+              </a>
+            </div>
           </div>
-          <div className={styles.footerMenu}>
-            <a onClick={() => { handleClick("industries"); }}>Industries</a>
-            <a onClick={() => { handleClick("workflow"); }}>Workflow</a>
-            <a onClick={() => { handleClick("pricing"); }}>Pricing</a>
-            <a href='https://platform.runhelix.ai/auth' target='_blank'>Login</a>
+          <div className={styles.line}></div>
+          <div className={styles.footerBottomText}>
+            <Link href="/privacy-policy">Privacy Policy</Link>
+            <Link href="/terms-and-conditions">Terms and Conditions</Link>
           </div>
-          <div className={styles.socialIcon}>
-            <a href='https://www.tiktok.com/notfound' target='_blank'>
-              <img src={TiktokIcon} alt='TiktokIcon' />
-            </a>
-            <img src={TwitterIcon} alt='TwitterIcon' />
-            <a href='https://www.linkedin.com/company/run-helix-ai' target='_blank'>
-              <img src={LinkdinIcon} alt='LinkdinIcon' />
-            </a>
-            <a href='https://www.youtube.com/@RunHelix_AI' target='_blank'>
-              <img src={YoutubeIcon} alt='YoutubeIcon' />
-            </a>
+          <div className={styles.footerText}>
+            <h4>
+              Helix
+            </h4>
           </div>
-        </div>
-        <div className={styles.line}></div>
-        <div className={styles.footerBottomText}>
-          <Link href="/privacy-policy">Privacy Policy</Link>
-          <Link href="/terms-and-conditions">Terms and Conditions</Link>
-        </div>
-        <div className={styles.footerText}>
-          <h4>
-            Helix
-          </h4>
         </div>
       </div>
       <div className={styles.bgbottom}></div>
