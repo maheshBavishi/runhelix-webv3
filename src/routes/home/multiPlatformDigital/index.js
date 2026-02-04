40082ab2527af690b6492b5998e649c7e70d6ab3
mahesh <dev030.rejoice@gmail.com>
2026-02-04 17:37:52 +0530

    video change

--- src/routes/home/multiPlatformDigital/index.js
+++ src/routes/home/multiPlatformDigital/index.js
@@ -7,9 +7,9 @@ const Video4 = '/assets/video/video4.mp4';
 const videoList = [
     "https://rnhlndmeazutnirvjzzt.supabase.co/storage/v1/object/public/my-videos/188e86eb-cd75-4877-b832-2d7b3a886b9e.mp4",
     "https://rnhlndmeazutnirvjzzt.supabase.co/storage/v1/object/public/my-videos/fba3f9fc-5a2b-4985-9269-42b0f0980589.mp4",
-    "https://rnhlndmeazutnirvjzzt.supabase.co/storage/v1/object/public/my-videos/5a4d0805-6934-45c8-aca7-17a15eb6b06f.mp4",
+    "https://rnhlndmeazutnirvjzzt.supabase.co/storage/v1/object/public/trim_videos/trimmed/e9b5668f-d707-49c8-88b4-edded5c18a86.mp4",
     "https://rnhlndmeazutnirvjzzt.supabase.co/storage/v1/object/public/my-videos/5366b959-1cc7-40f3-86a2-f4006019492e.mp4",
-    "https://rnhlndmeazutnirvjzzt.supabase.co/storage/v1/object/public/my-videos/7d3bb1a8-24c2-4869-a83d-c4d5c69cc7c7.mp4",
+    "https://rnhlndmeazutnirvjzzt.supabase.co/storage/v1/object/public/trim_videos/trimmed/cd9c13b1-9623-4419-bdde-6e1a9e6c17fd.mp4",
     "https://rnhlndmeazutnirvjzzt.supabase.co/storage/v1/object/public/my-videos/49eb288f-f239-45a6-be92-11a96a39fd2e.mp4",
     "https://rnhlndmeazutnirvjzzt.supabase.co/storage/v1/object/public/my-videos/b3f0f9c8-11a1-4382-aa38-3fcf0ad1e0ab.mp4",
     "https://rnhlndmeazutnirvjzzt.supabase.co/storage/v1/object/public/my-videos/a4803275-c9e9-42ad-bcab-7793bc27e612.mp4",
